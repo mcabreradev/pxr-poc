f1eb45e495f2792ea1e48c0d22eb399fc599bc25
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2023-07-04 19:27:21 -0300

    chore: update nextjs sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,9 +3,7 @@
  * @see https://github.com/iamvishnusankar/next-sitemap#readme
  */
 module.exports = {
-  // !STARTERCONF Change the siteUrl
-  /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  siteUrl: 'https://tsnext-tw.thcl.dev',
+  siteUrl: process.env.PAXER_APP_URL || 'http://localhost:3000',
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [{ userAgent: '*', allow: '/' }],

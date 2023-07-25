5f4d50e4937d99c99cb54eaf415058e90f920db0
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2023-07-25 16:36:15 -0300

    fix: typography tests and snapshot

--- src/components/typography/typography.test.tsx
+++ src/components/typography/typography.test.tsx
@@ -2,11 +2,13 @@
 import 'jest-styled-components';
 import renderer from 'react-test-renderer';
 
-import { Text } from '.';
+import Typography, { TextVariant } from './';
 
 describe('Text component', () => {
   it('should renders Text component correctly', () => {
-    const tree = renderer.create(<Text>Hello Paxer!</Text>).toJSON();
+    const tree = renderer
+      .create(<Typography variant={TextVariant.base}>Hello Paxer!</Typography>)
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 });

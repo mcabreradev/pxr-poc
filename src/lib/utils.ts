e5eda98266b2290fbd1669f27b360baee2b651a1
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2023-07-31 18:34:02 -0300

    chore(utils) update utils libs

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
-import { type ClassValue, clsx } from 'clsx';
+/* eslint-disable simple-import-sort/imports */
+import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {

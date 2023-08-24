4db51e924dda9be35f15abfa5fc2234c54d6e7fc
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2023-08-24 14:22:29 -0300

    feat(PXR-1116): api response typing

--- src/lib/pegaso.ts
+++ src/lib/pegaso.ts
@@ -1,6 +1,27 @@
 import axios from 'axios';
 import qs from 'qs';
 
+// Define un tipo para los datos del error
+type ApiErrorData = {
+  code: number;
+  message: string;
+};
+
+// Define un tipo para el error completo
+type ApiError = {
+  response: {
+    status: number;
+    data: ApiErrorData;
+  };
+};
+
+// type ApiResponse = {
+//   status: string;
+//   code: number;
+//   data: unknown;
+//   message: string;
+// };
+
 const getAccessToken = async () => {
   const data = {
     grant_type: 'client_credentials',
@@ -20,9 +41,28 @@ const getAccessToken = async () => {
     const res = await axios(options);
     return res.data.access_token;
   } catch (error) {
-    throw new Error(
-      "Can't get token from " + process.env.PAXER_AUTH_URL + ' ' + error,
-    );
+    const apiError = error as ApiError;
+
+    if (apiError.response) {
+      const { status, data } = apiError.response;
+      if (status === 404) {
+        // eslint-disable-next-line no-console
+        console.log('Recurso no encontrado:', data.message);
+      } else if (status === 500) {
+        // eslint-disable-next-line no-console
+        console.log('Error interno del servidor:', data.message);
+      } else {
+        // eslint-disable-next-line no-console
+        console.log('Error desconocido:', data.message);
+      }
+    } else {
+      // eslint-disable-next-line no-console
+      console.log('Error de red:', error);
+    }
+    // throw new Error(
+    //   "Can't get token from " + process.env.PAXER_AUTH_URL + ' ' + error,
+    // );
+    throw error;
   }
 };
 
@@ -45,6 +85,26 @@ export const get = async (url: string) => {
     const res = await axios(options);
     return res.data;
   } catch (error) {
-    throw new Error("Can't fetch data" + error);
+    const apiError = error as ApiError;
+
+    if (apiError.response) {
+      const { status, data } = apiError.response;
+      if (status === 404) {
+        // eslint-disable-next-line no-console
+        console.log('Recurso no encontrado:', data.message);
+      } else if (status === 500) {
+        // eslint-disable-next-line no-console
+        console.log('Error interno del servidor:', data.message);
+      } else {
+        // eslint-disable-next-line no-console
+        console.log('Error desconocido:', data.message);
+      }
+    } else {
+      // eslint-disable-next-line no-console
+      console.log('Error de red:', error);
+    }
+
+    // throw new Error("Can't fetch data" + error);
+    return apiError.response.data;
   }
 };

878d49c55d3e9afec6f22fa0dc1880fc93f87846
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2023-09-04 17:43:34 -0300

    feat(PXR-1119): add trycatch to Route App

--- src/app/api/property/route.ts
+++ src/app/api/property/route.ts
@@ -3,8 +3,11 @@ import { NextRequest, NextResponse } from 'next/server';
 import api from '@/lib/pegaso';
 
 export async function GET(request: NextRequest) {
-  const propertyId = request.nextUrl.searchParams.get('propertyId');
-  const res = await api.get(`/property/${propertyId}`);
-
-  return NextResponse.json(res.property);
+  try {
+    const propertyId = request.nextUrl.searchParams.get('propertyId');
+    const res = await api.get(`/property/${propertyId}`);
+    return NextResponse.json(res.property);
+  } catch (error) {
+    return NextResponse.error();
+  }
 }
--- src/app/api/room-type/route.ts
+++ src/app/api/room-type/route.ts
@@ -4,10 +4,13 @@ import { getFirstOwnProperty } from '@/lib/helper';
 import api from '@/lib/pegaso';
 
 export async function GET(request: NextRequest) {
-  const propertyId = request.nextUrl.searchParams.get('propertyId');
-  const roomTypeId = request.nextUrl.searchParams.get('roomTypeId');
-  const res = await api.get(`/property/${propertyId}/roomtype/${roomTypeId}`);
-  const room = getFirstOwnProperty(res.roomTypes);
-
-  return NextResponse.json(room);
+  try {
+    const propertyId = request.nextUrl.searchParams.get('propertyId');
+    const roomTypeId = request.nextUrl.searchParams.get('roomTypeId');
+    const res = await api.get(`/property/${propertyId}/roomtype/${roomTypeId}`);
+    const room = getFirstOwnProperty(res.roomTypes);
+    return NextResponse.json(room);
+  } catch (error) {
+    return NextResponse.error();
+  }
 }

6d53353501d46790dcf7bc30af8e984e135c9dd1
Diego Peña <djpg98@gmail.com>
2024-02-20 12:00:42 -0400

    feat: added map and links

--- src/features/summary/page.tsx
+++ src/features/summary/page.tsx
@@ -20,7 +20,8 @@ import useFetchProperty from '@/queries/use-property';
 import useRoomTypeQuery from '@/queries/use-roomtype';
 
 import data from '../payment/data.json';
-require('dayjs/locale/es'); //This require is necessary to get the weekday name in correct language
+import additionalData from '../property/data.json';
+require('dayjs/locale/es'); //This require is necessary to get the weekday name in the correct language
 
 type Props = {
   className?: string;
@@ -112,150 +113,182 @@ export default function SummaryFeature({ className, roomtype }: Props) {
               <Typography variant='sm' className='ml-1 text-white'>
                 {property.reviewRatingCount
                   ? `${property.reviewRatingScore} (${property.reviewRatingCount})`
-                  : 'No reviews yet'}
+                  : t('summary.no-reviews')}
               </Typography>
             </div>
           </div>
-          <div className='mb-2 flex w-full flex-row divide-x px-0.5'>
-            <div className='basis-1/2 px-4 py-2'>
-              <Typography variant='sm' weight='bold'>
-                {t('checkin')}
-              </Typography>
-              <Typography variant='sm' weight='normal'>
-                {capitalize(
-                  checkin.locale(i18n.language).format('dddd, DD MMM YYYY'),
-                )}
-              </Typography>
-              <Typography variant='sm' weight='normal'>
-                {formatTime(property.checkInTime)}
-              </Typography>
-            </div>
-            <div className='basis-1/2 px-4 py-2'>
-              <Typography variant='sm' weight='bold'>
-                {t('checkout')}
-              </Typography>
-              <Typography variant='sm' weight='normal'>
-                {capitalize(
-                  checkout.locale(i18n.language).format('dddd, DD MMM YYYY'),
-                )}
-              </Typography>
-              <Typography variant='sm' weight='normal'>
-                {formatTime(property.checkOutTime)}
-              </Typography>
-            </div>
-          </div>
-          <div className='mx-4 border-b'></div>
-          <SummaryRow
-            leftMainText={t('summary.address')}
-            leftSecondaryText={`${property.street}, ${property.city}, ${property.state}, ${property.countryName}`}
-            rightMainText={t('summary.directions')}
-            rightMainTag='a'
-          />
-          <SummaryRow
-            leftMainText={t('summary.guests')}
-            leftSecondaryText={passengers.toString()}
-            rightMainText={t('summary.fill-data')}
-            rightMainTag='a'
-          />
-          <SummaryRow
-            leftMainText={t('summary.cost')}
-            rightMainText={`$ ${reservation.total}`}
-            className='mb-6'
-          />
-          <SummaryRow
-            leftMainText={t('summary.reservation-code')}
-            rightMainText='SFFE3553'
-            className='mb-5'
-          />
-          <div className='mx-4 border-b'></div>
-          <div className='mx-4 my-5 flex flex-row'>
-            <Typography variant='sm' tag='a' className='text-black'>
-              {t('summary.contact')}
-            </Typography>
-            <Icon
-              variant='internet'
-              color='#757575'
-              height='17px'
-              width='17px'
-              className='ml-1 mt-1'
-            />
-            <Icon
-              variant='email'
-              color='#757575'
-              height='20px'
-              width='20px'
-              className='ml-1 mt-0.5'
-            />
-            <Icon
-              variant='phone'
-              color='#757575'
-              height='20px'
-              width='20px'
-              className='ml-1 mt-0.5'
-            />
-            <Icon
-              variant='whatsapp'
-              height='20px'
-              width='20px'
-              className='ml-1 mt-0.5'
-            />
-          </div>
-          <div className='mx-4 border-b'></div>
-          <div className='mx-4 my-5 flex flex-row'>
-            <Typography variant='sm' tag='a' className='text-black'>
-              {t('summary.hotel-information')}
-            </Typography>
-          </div>
-          <HR />
-          <PriceDetails
-            room={room}
-            reservation={reservation}
-            extra={extra}
-            plan={plan}
-            checkin={checkin}
-            checkout={checkout}
-          />
-          <HR />
-          <section>
-            <Typography variant='h2' weight='normal' className='px-4'>
-              {t('summary.payment-title')}
-            </Typography>
-            {payment.amount ? (
+          <div className='flex flex-row'>
+            <div className='md:basis-1/3'>
+              <div className='mb-2 flex w-full flex-row divide-x px-0.5'>
+                <div className='basis-1/2 px-4 py-2'>
+                  <Typography variant='sm' weight='bold'>
+                    {t('checkin')}
+                  </Typography>
+                  <Typography variant='sm' weight='normal'>
+                    {capitalize(
+                      checkin.locale(i18n.language).format('dddd, DD MMM YYYY'),
+                    )}
+                  </Typography>
+                  <Typography variant='sm' weight='normal'>
+                    {formatTime(property.checkInTime)}
+                  </Typography>
+                </div>
+                <div className='basis-1/2 px-4 py-2'>
+                  <Typography variant='sm' weight='bold'>
+                    {t('checkout')}
+                  </Typography>
+                  <Typography variant='sm' weight='normal'>
+                    {capitalize(
+                      checkout
+                        .locale(i18n.language)
+                        .format('dddd, DD MMM YYYY'),
+                    )}
+                  </Typography>
+                  <Typography variant='sm' weight='normal'>
+                    {formatTime(property.checkOutTime)}
+                  </Typography>
+                </div>
+              </div>
+              <div className='mx-4 border-b'></div>
               <SummaryRow
-                leftMainText={`$ ${payment.amount} ${payment.currency}`}
-                rightMainText={t('summary.invoice')}
+                leftMainText={t('summary.address')}
+                leftSecondaryText={`${property.street}, ${property.city}, ${property.state}, ${property.countryName}`}
+                rightMainText={t('summary.directions')}
                 rightMainTag='a'
-                className='mb-5'
               />
-            ) : (
               <SummaryRow
-                leftMainText={t('summary.no-payment')}
-                rightMainText=''
+                leftMainText={t('summary.guests')}
+                leftSecondaryText={passengers.toString()}
+                rightMainText={t('summary.fill-data')}
+                rightMainTag='a'
+              />
+              <SummaryRow
+                leftMainText={t('summary.cost')}
+                rightMainText={`$ ${reservation.total}`}
+                className='mb-6'
+              />
+              <SummaryRow
+                leftMainText={t('summary.reservation-code')}
+                rightMainText='SFFE3553'
                 className='mb-5'
               />
-            )}
-          </section>
-          <HR />
-          <Cancellation plan={plan} />
-          <HR />
-          <section>
-            <div className='px-4'>
-              <Typography variant='h2' weight='normal'>
-                {t('info.taxes-details')}
-              </Typography>
-              <div className='my-3' />
-              <Typography variant='sm' className='text-neutral-500'>
-                {t('summary.taxes-reminder-1')} ${reservation.taxes}{' '}
-                {payment.currency} {t('summary.taxes-reminder-2')}
-              </Typography>
+              <div className='mx-4 border-b'></div>
+              <div className='mx-4 my-5 flex flex-row'>
+                <Typography variant='sm' tag='a' className='text-black'>
+                  {t('summary.contact')}
+                </Typography>
+                <a href={additionalData.contact.web}>
+                  <Icon
+                    variant='internet'
+                    color='#757575'
+                    height='17px'
+                    width='17px'
+                    className='ml-1 mt-1'
+                  />
+                </a>
+                <a href={`mailto:${additionalData.contact.email}`}>
+                  <Icon
+                    variant='email'
+                    color='#757575'
+                    height='20px'
+                    width='20px'
+                    className='ml-1 mt-0.5'
+                  />
+                </a>
+                <a href={`tel:${additionalData.contact.phone}`}>
+                  <Icon
+                    variant='phone'
+                    color='#757575'
+                    height='20px'
+                    width='20px'
+                    className='ml-1 mt-0.5'
+                  />
+                </a>
+                <a href={`https://wa.me/${additionalData.contact.whatsapp}`}>
+                  <Icon
+                    variant='whatsapp'
+                    height='20px'
+                    width='20px'
+                    className='ml-1 mt-0.5'
+                  />
+                </a>
+              </div>
+              <div className='mx-4 border-b'></div>
+              <div className='mx-4 my-5 flex flex-row'>
+                <Typography variant='sm' tag='a' className='text-black'>
+                  {t('summary.hotel-information')}
+                </Typography>
+              </div>
+              <HR />
+              <PriceDetails
+                room={room}
+                reservation={reservation}
+                extra={extra}
+                plan={plan}
+                checkin={checkin}
+                checkout={checkout}
+              />
+              <HR />
+              <section>
+                <Typography variant='h2' weight='normal' className='px-4'>
+                  {t('summary.payment-title')}
+                </Typography>
+                {payment.amount ? (
+                  <SummaryRow
+                    leftMainText={`$ ${payment.amount} ${payment.currency}`}
+                    rightMainText={t('summary.invoice')}
+                    rightMainTag='a'
+                    className='mb-5'
+                  />
+                ) : (
+                  <SummaryRow
+                    leftMainText={t('summary.no-payment')}
+                    rightMainText=''
+                    className='mb-5'
+                  />
+                )}
+              </section>
+              <HR />
+              <Cancellation plan={plan} />
+              <HR />
+              <section>
+                <div className='px-4'>
+                  <Typography variant='h2' weight='normal'>
+                    {t('info.taxes-details')}
+                  </Typography>
+                  <div className='my-3' />
+                  <Typography variant='sm' className='text-neutral-500'>
+                    {t('summary.taxes-reminder-1')} ${reservation.taxes}{' '}
+                    {payment.currency} {t('summary.taxes-reminder-2')}
+                  </Typography>
+                </div>
+              </section>
+              <HR />
+              <section className='px-4'>
+                <div className='py-0 pb-7'>
+                  <HotelRules rules={data.rules} />
+                </div>
+              </section>
             </div>
-          </section>
-          <HR />
-          <section className='px-4'>
-            <div className='py-0 pb-7'>
-              <HotelRules rules={data.rules} />
+            <div className='hidden md:block md:basis-2/3 md:pb-7'>
+              <section className='h-full'>
+                <div className='h-full pl-4'>
+                  <iframe
+                    title={t('title.exact-location')}
+                    loading='lazy'
+                    className='h-full w-full'
+                    src={additionalData.map.url}
+                    width='100%'
+                    height='300'
+                    style={{ border: '0' }}
+                    allowFullScreen
+                    aria-hidden='false'
+                    tabIndex={0}
+                  ></iframe>
+                </div>
+              </section>
             </div>
-          </section>
+          </div>
         </div>
       </div>
     </Container>

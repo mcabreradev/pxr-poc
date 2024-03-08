6c2f34fa0a3f3180f29bdb406556bb01d22d7883
Diego Peña <djpg98@gmail.com>
2024-03-08 17:35:21 -0400

    wip: completed types and semi-hardcoded set reservation request in store

--- src/features/payment/page.tsx
+++ src/features/payment/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+/* eslint-disable simple-import-sort/imports */
 import { redirect } from 'next/navigation';
 import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
@@ -10,14 +11,18 @@ import { cn } from '@/lib/utils';
 import BackButton from '@/components/common/back-button';
 
 import { useSessionStore, useUserStore } from '@/store';
+import useReservationRequestStore from '@/store/use-reservation-request.store';
 
 import NotConnected from '@/app/not-connected';
 import { ERRORS, URL } from '@/constants';
 import StripePayment from '@/features/payment/strype-payment';
 import { usePropertyQuery, useRoomTypeQuery } from '@/queries';
 
+import useSearchParamOrStore from '../../hooks/use-search-param-or-store';
 import MyTripDetails from './my-trip-details';
 import SkeletonComponent from './skeleton';
+
+import { ReservationRequest } from '@/types';
 
 type Props = {
   roomTypeId: number;
@@ -36,7 +41,9 @@ export default function PaymentFeature({ roomTypeId, action }: Props) {
     data: room,
   } = useRoomTypeQuery(roomTypeId);
   const { setLoginEnabled } = useUserStore();
+  const { setReservationRequest } = useReservationRequestStore();
   const { session } = useSessionStore();
+  const { getAdults, getCheckin, getCheckout } = useSearchParamOrStore();
 
   const actionPayment = !action;
   const actionSuccess = action === URL.SUCCESS;
@@ -49,6 +56,49 @@ export default function PaymentFeature({ roomTypeId, action }: Props) {
       setLoginEnabled(true);
     };
   }, [setLoginEnabled]);
+
+  useEffect(() => {
+    if (session) {
+      const reservationRequest: ReservationRequest = {
+        property_id: property.id,
+        guest_id: session.sub,
+        sales_channel_type: 'web',
+        process_state: 'WAITING_FOR_PAYMENT',
+        date_in: getCheckin(),
+        date_out: getCheckout(),
+        mon_id: 1,
+        mon_iso: 'USD',
+        total_cost: 0,
+        room_types_cost: 0,
+        guest_mon_iso: '',
+        mon_commission_id: 0,
+        commission_mon_iso: '',
+        is_default_commission: 0,
+        reservation_status: 'WO_PAYMENT',
+        room_types: [],
+        extras: [],
+        coupons: [],
+        adults_amount: getAdults(),
+        additional_fields_values: '',
+        reg_status: 'active',
+        sales_origin_type: 'DIRECT',
+        send_confirmed_email: 1,
+        confirmed_email_active: 1,
+        thank_you_email_to_pax_active: 1,
+        send_payment_email: 1,
+        new_booking_email_send_to_hotel: 1,
+        confirmed_agreement: 0,
+      };
+      setReservationRequest(reservationRequest);
+    }
+  }, [
+    getAdults,
+    getCheckin,
+    getCheckout,
+    property,
+    session,
+    setReservationRequest,
+  ]);
 
   if (!session) {
     redirect('/');
--- src/types/index.ts
+++ src/types/index.ts
@@ -221,9 +221,9 @@ export type DetailedPassengerInfo = {
 export type ReservedRoom = {
   har_in: string;
   har_out: string;
-  har_hab_id?: number | null; // Physical id of the enum, does not have to be sent
+  har_hab_id?: number | null; // Physical id of the room, does not have to be sent
   har_tha_id: number; //Room type id
-  har_res_id: number;
+  har_res_id?: number | null;
   har_pla_id: number; // Selected plan
   har_cli_id: number;
   har_hot_id: number;
@@ -231,40 +231,15 @@ export type ReservedRoom = {
   har_seniors?: number | null;
   har_children: number;
   har_infants: number;
-  har_pax_info: string;
-  har_adults_info: string;
+  har_pax_info?: string | null;
+  har_adults_info?: string | null;
   har_childrens_info: string;
   har_seniors_info?: string | null;
-  har_infants_info: string;
-  har_canc_reason?: string | null; // I don't think we need this one, but technically it's an Enum
+  har_infants_info: string | null;
   har_cost: number;
-  har_commission: number;
-  har_commission_amount: number;
-  har_commission_percentage: number;
-  har_commission_currency: string;
-  har_fee_amount: number;
-  har_fee_currency: string;
-  har_fee_description: string;
-  har_fee_includes_taxes: number;
-  har_tax_amount: number;
-  har_tax_currency: string;
-  har_cancel_amount: number;
-  har_surchage: number;
-  har_surchage_currency: string;
-  har_check_in: Date;
-  har_check_out: Date;
-  har_last_date_account_closed: string;
   har_additional_field_1?: string | null;
   har_additional_field_2?: string | null;
   har_additional_field_3?: string | null;
-  har_overbooking: number;
-  har_rcld_channel_commission: number;
-  har_channel_additional_information: string;
-  har_removed_from_the_booking: number;
-  har_cancellation_date: Date;
-  har_cancellation_user: number;
-  har_status_reg: string;
-  har_updated_by: number;
 };
 
 export type CMAValue = {
@@ -281,9 +256,19 @@ export type Extras = {
   cma_values: CMAValue[];
 };
 
+export type Coupon = {
+  cpn_id: number;
+  implementation_type: string;
+  mon_id: number;
+  amount: number;
+  amount_applied: number;
+  use_with_others: number;
+};
+
 export type ReservationRequest = {
+  id?: number | null;
   property_id: number;
-  guest_id: string;
+  guest_id: string | undefined;
   sales_channel_type: string;
   process_state: string;
   date_in: string; // Like this: 2024-03-07
@@ -294,12 +279,13 @@ export type ReservationRequest = {
   room_types_cost: number;
   guest_mon_iso: string;
   mon_commission_id: number;
+  commission_mon_iso: string;
   is_default_commission: number;
   reservation_status: string;
   room_types: ReservedRoom[];
-  extras: Extras[];
-  //
-  adults_amounts: number;
+  extras?: Extras[];
+  coupons?: Coupon[];
+  adults_amount: number;
   additional_fields_values: string;
   reg_status: string;
   sales_origin_type: string;
@@ -309,6 +295,72 @@ export type ReservationRequest = {
   send_payment_email: number;
   new_booking_email_send_to_hotel: number;
   confirmed_agreement: number;
+  guest_preferred_language?: string;
+  guest_email?: string;
+  guest_country_code?: string;
+  payment_id?: number;
+};
+
+// Names come like this directly from backend
+export type Vq = {
+  Qp?: string;
+  Sp?: string;
+  Yp?: number;
+  Np?: number;
+  Lp?: number;
+  Op?: number;
+  Pp?: number;
+  Vp?: number;
+  Xp?: number;
+  Rp?: number;
+  Wp?: number;
+  Mp?: number;
+  Tp?: string;
+};
+
+// Names come like this directly from backend
+export type ReservationResponse = {
+  xq?: number;
+  res_guest_id?: number;
+  Lq?: string;
+  iq?: string;
+  zq?: string;
+  Eq?: string;
+  oq?: number;
+  qq?: number;
+  res_status_reg?: string;
+  pq?: number;
+  Cq?: number;
+  res_guest_mon_iso?: string;
+  Dq?: string;
+  res_mon_commission?: number;
+  sq?: number;
+  vq?: Vq[];
+  uq?: never[];
+  res_commission_default?: number;
+  gq?: string;
+  un?: number;
+  res_send_confirmed_email?: number;
+  res_send_payment_email?: number;
+  res_new_booking_email_send_to_hotel?: number;
+  hq?: number;
+};
+
+export type ReservationRequestResponseData = {
+  id: number;
+  process_state: string;
+  step_completed: string;
+  reservation_created: number;
+  reservation_id?: number;
+  id_public?: string;
+  client_id?: number;
+  reservation?: ReservationResponse;
+};
+
+export type ReservationRequestResponse = {
+  message: string;
+  code: number;
+  data: ReservationRequestResponseData | ReservationRequest | string | number;
 };
 
 export type { Ratesplan };

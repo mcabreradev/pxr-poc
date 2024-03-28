8843619d14fe455ca3bc3452577d462d24bde5f8
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2024-03-28 17:31:28 -0300

    fix: update Stripe import in strype-payment.tsx

--- src/features/payment/strype-payment.tsx
+++ src/features/payment/strype-payment.tsx
@@ -1,7 +1,6 @@
 import { Elements } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
+import { loadStripe, Stripe } from '@stripe/stripe-js';
 import { memo, useEffect } from 'react';
-import Stripe from 'stripe';
 
 import { uuid } from '@/lib/utils';
 

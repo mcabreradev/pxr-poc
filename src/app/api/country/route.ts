3020af233b617e35fa1f840e8d8dafd17729bd9d
Diego Peña <djpg98@gmail.com>
2024-04-04 15:45:38 -0400

    style: replaced hardcoded string with constant

--- src/app/api/country/route.ts
+++ src/app/api/country/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+import { SUCCESS } from '@/constants';
+
 export async function GET(request: NextRequest) {
   const country = request.headers.get('cloudfront-viewer-country') ?? '';
-  return NextResponse.json({ status: 'success', data: { country: country } });
+  return NextResponse.json({ status: SUCCESS, data: { country: country } });
 }

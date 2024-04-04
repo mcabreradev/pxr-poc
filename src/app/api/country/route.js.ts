4433b2384fb413dd008b2e1bbbd837c73ee60751
Diego Peña <djpg98@gmail.com>
2024-04-04 11:24:11 -0400

    feat: corrected header name

--- src/app/api/country/route.js.ts
+++ src/app/api/country/route.js.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
-  const country = request.headers.get('cf-ipcountry') ?? '';
+  const country = request.headers.get('cloudfront-viewer-country') ?? '';
   return NextResponse.json({ status: 'success', data: { country: country } });
 }

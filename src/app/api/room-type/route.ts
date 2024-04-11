ece9d81a6d4b7bdb462a4aa110126871f9521cbd
Diego Peña <djpg98@gmail.com>
2024-04-11 17:19:44 -0400

    fix: api routes aren't hardcoded anymore

--- src/app/api/room-type/route.ts
+++ src/app/api/room-type/route.ts
@@ -7,7 +7,7 @@ export async function GET(request: NextRequest) {
   const propertyId = request.nextUrl.searchParams.get('propertyId');
   const roomTypeId = request.nextUrl.searchParams.get('roomTypeId');
   const res = await api.get(
-    `/v1/dev/property/${propertyId}/roomtype/${roomTypeId}`,
+    `/v1/:environment/property/${propertyId}/roomtype/${roomTypeId}`,
   );
   const room = getFirstOwnProperty(res.roomTypes);
 
